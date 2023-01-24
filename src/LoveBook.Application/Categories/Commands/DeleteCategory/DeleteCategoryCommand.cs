7d24c102393b8e7ef54a4e31dd6cf6a34bbff994
Vahid Cheshmy <v.cheshmi@gmail.com>
2023-01-24 12:30:22 +0100

    Delete category.

---
+++ src/LoveBook.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace LoveBook.Application.Categories.Commands.DeleteCategory;
+
+public record DeleteCategoryCommand(long CategoryId):IRequest<Result<DeleteCategoryPayload>>;
+
+public record DeleteCategoryPayload(long? Id,string[]? Errors = null);
